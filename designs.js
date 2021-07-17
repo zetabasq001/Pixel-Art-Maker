8a223c5033c2910db08c082c875950dd547bcc7d
George <gzald001@gmail.com>
2021-07-17 10:48:08 -0400

    Select size input

--- designs.js
+++ designs.js
@@ -2,6 +2,8 @@
 const colorInput = document.querySelector('#colorPicker');
 
 // Select size input
+const sizeInputHeight = document.querySelector('#inputHeight');
+const sizeInputWidth = document.querySelector('#inputWidth');
 
 // When size is submitted by the user, call makeGrid()
 
