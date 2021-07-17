da907ab92f942746bbcf8989c3d4a45b42bee5f4
George <gzald001@gmail.com>
2021-07-17 11:46:32 -0400

    feat: Add event listeners for size input

--- designs.js
+++ designs.js
@@ -6,9 +6,15 @@ colorInput.addEventListener('input', function(event){
 });
 
 // Select size input
+var sizeSide;
 const sizeInputHeight = document.querySelector('#inputHeight');
 const sizeInputWidth = document.querySelector('#inputWidth');
+function sizeInput(event){
+  sizeSide = event.target.value;
+}
 
+sizeInputHeight.addEventListener('input', sizeInput);
+sizeInputWidth.addEventListener('input', sizeInput);
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
